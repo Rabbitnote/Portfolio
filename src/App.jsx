df54df0a8ae57a62ad87da42733707d01319940a
thanakorn <thanakorn.aun@innovasive.co.th>
2024-04-05 23:51:26 +0700

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-10 bg-primary">
+        <div className="bg-hero-pattern bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-10">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
